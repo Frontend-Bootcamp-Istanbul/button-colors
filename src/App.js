13e6cc56f1d35e3cee3fb5dd5041b0085606f22f
Mehmet Yurtar <mehmetyurtar@gmail.com>
2019-11-17 15:45:56 +0300

    button colors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RenkliButon from "./RenkliButon";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            guncelRenk: {}
+        }
+
+        this.renkDegistir = this.renkDegistir.bind(this);
+    }
+
+    renkDegistir(butonunRengi){
+        console.log(butonunRengi);
+        this.setState({
+            guncelRenk: butonunRengi
+        })
+    }
+
+    render(){
+        let mainClass = "main-container ";
+        mainClass += this.state.guncelRenk.style;
+       return <div className={mainClass}>
+            <div className="main-container">
+                {this.state.guncelRenk.renk} Buton Tıklandı
+            </div>
+           {
+               [0,1,2,3,4].map(() => {
+                   return <RenkliButon renkDegistir={this.renkDegistir} />
+               })
+           }
+        </div>
+    }
+
 }
 
 export default App;
